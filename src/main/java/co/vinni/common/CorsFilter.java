43b2641b33b795dcaf5ac00ab8af3a363d03f25b
Julian Martinez <juliand.martinez12@gmail.com>
2026-05-26 23:03:07 -0500

    fix: agregar CorsFilter manual para resolver CORS en produccion

---
+++ src/main/java/co/vinni/common/CorsFilter.java
@@ -0,0 +1,21 @@
+package co.vinni.common;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "86400");
+    }
+}
